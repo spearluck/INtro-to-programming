8e510fd9136b5558557d004040aed08b40c37aa8
Vincent <93230173+spearluck@users.noreply.github.com>
2026-01-21 13:45:19 +0200

    Delete Source_Code directory

--- Source_Code/Chapter_8/Class-Work/2.return.js
+++
@@ -1,9 +0,0 @@
-// Create a function and store it in a variable called 'double'
-var two = function (number) {
-    // The function takes one input (parameter) called 'number'
-
-    // It returns the result of multiplying that number by 2
-    return number * 2;
-};
-
-console.log(two(5));
